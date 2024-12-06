950cab6e870a2616c48b64b1c2bd06949cf9f645
tommyhertanto <tommy.hertanto21@gmail.com>
2024-12-06 08:30:57 +0700

    Tugas hari ke-4

--- components/header/index.js
+++ components/header/index.js
@@ -1,3 +1,3 @@
 export default function Header() {
-    return <div>Header</div>
+    return <div className="text-sm text-blue-500">Header</div>
     }
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Header from "@/components/header";
 import Footer from "@/components/footer";
 import Content from "@/components/content";
 
+
 export default function Layout({ children, metaTitle, metaDescription }) {
  return (
   <div>

78dd07c0cd66f365da14d627b8c07d098f794f59
tommyhertanto <tommy.hertanto21@gmail.com>
2024-12-13 07:58:28 +0700

    Tugas hari ke-9

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+export default async function handler(req, res) {
+  // res.status(200).json({ name: "John Doe" });
+  try {
+    const response = await (await fetch("https://jsonplaceholder.typicode.com/users")).json();
+  res.status(200).json({ ...response });
+} catch (error) {}    
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
 import Layout from "@/layout";
+import dynamic from "next/dynamic";
+import { useEffect } from "react";
 
 export default function Main() {
+   useEffect(() => {
+      fetch("/api/hello")
+      .then(res => res.json())
+      .then(res => console.log("response => ", res))
+      .catch(err => console.log("error => ", err));
+   }, []);
  return (
   <div>
    <Layout metaTitle="Home">
